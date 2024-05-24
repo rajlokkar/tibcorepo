eb9e8a6d79b7d7901cff3782338cea031a594cfd
rajlokkar <rajeshtibco7@gmail.com>
2024-05-24 12:45:46 +0000

    new code base

---
+++ test1.module/Tests/A3DEWS2RF4.ml
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="ASCII"?>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <CurrentLoadedTest href="platform:/resource/test1.module/Tests/ReceiverTest.bwt#/"/>
+  <EmulationDataFile>ACED00057400105265636569766572546573742E627774</EmulationDataFile>
+</emulation:BWTFile>
