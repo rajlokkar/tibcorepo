305a2da07eb5cdbc200eba306c2f9a148f8dd3bb
rajlokkar <rajeshtibco7@gmail.com>
2024-05-26 10:40:36 +0000

    new files

--- test1.module/Tests/A3DEWS2RF4.ml
+++ test1.module/Tests/A3DEWS2RF4.ml
@@ -2,4 +2,5 @@
 <emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
   <CurrentLoadedTest href="platform:/resource/test1.module/Tests/ReceiverTest.bwt#/"/>
   <EmulationDataFile>ACED00057400105265636569766572546573742E627774</EmulationDataFile>
+  <EmulationDataFile>ACED000574001D5C74657374312E6D6F64756C655C54657374735C74657374322E627774</EmulationDataFile>
 </emulation:BWTFile>
